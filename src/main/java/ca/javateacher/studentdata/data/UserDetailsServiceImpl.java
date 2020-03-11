24fc3d89d473579c06f3c2fd3a86e30965229b65
Alex Tetervak <tetervak@sheridancollege.ca>
2020-03-11 17:36:50 -0400

    removed an unused import

--- src/main/java/ca/javateacher/studentdata/data/UserDetailsServiceImpl.java
+++ src/main/java/ca/javateacher/studentdata/data/UserDetailsServiceImpl.java
@@ -2,7 +2,6 @@ package ca.javateacher.studentdata.data;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
